3848dee58c408bd7dd22dae1332b045427002b1d
Victor Grechi <victor_grechi@hotmail.com>
2022-11-04 14:48:25 -0300

    Allure reports setup

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources",
-        plugin = {"pretty", "html:target/cucumber/cucumber-report.html"},
+        plugin = {"pretty", "html:target/cucumber/cucumber-report.html", "junit:target/cucumber/cucumber-report.xml"},
         glue = {"steps"},
         monochrome = true)
 public class CucumberRunner {
